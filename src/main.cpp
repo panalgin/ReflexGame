a3213cc7178523a4de9af66c4d78fc075e8e319f
panalgin <daflan.official@gmail.com>
2019-12-15 14:35:51 +0300

    some dev

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #define LCD_I2C_ADDR 0x27
 #define LCD_COLS 20
 #define LCD_ROWS 4
+#define PREGAME_COUNTER 9
 
 /* #region State Machine */
 enum class GameState
@@ -19,19 +20,26 @@ LiquidCrystal_I2C lcd(LCD_I2C_ADDR, LCD_COLS, LCD_ROWS);
 char brandFirst[LCD_COLS + 1] = "    REFLEX OYUNU    ";
 char brandSecond[LCD_COLS + 1] = "      v: 0.92b      ";
 
-const uint8_t redPin = 3;
-const uint8_t greenPin = 4;
-const uint8_t bluePin = 5;
+const uint8_t redPin = 6;
+const uint8_t greenPin = 5;
+const uint8_t bluePin = 3;
 
-const uint8_t buttonPin = 5;
+const uint8_t buttonPin = 8;
 
 GameState State;
 bool screenNeedsUpdate = false;
+uint8_t pregameCounter, oldPregameCounter = 0;
 
 void watermark();
 void checkState();
+void checkInput();
 void showScores();
+void showPregame();
+void startCountdown();
+void startGame();
 void printWithAnimation(char bucket[LCD_ROWS][LCD_COLS + 1]);
+void setupTimer();
+void partialUpdates();
 
 void setup()
 {
@@ -48,9 +56,16 @@ void setup()
   lcd.clear();
   lcd.setCursor(0, 0);
 
+  setupTimer();
   watermark();
 
   screenNeedsUpdate = true;
+}
+
+void loop()
+{
+  checkState();
+  checkInput();
 }
 
 void watermark()
@@ -79,26 +94,52 @@ void watermark()
   delay(2000);
 }
 
-void loop()
-{
-  // put your main code here, to run repeatedly:
-
-  checkState();
-}
-
 void checkState()
 {
   switch (State)
   {
-    case GameState::None:
+  case GameState::None:
+  {
+    if (screenNeedsUpdate)
     {
-      if (screenNeedsUpdate) {
-        showScores();
-        screenNeedsUpdate = false;
-      }
-
-      break;
+      showScores();
+      screenNeedsUpdate = false;
     }
+
+    break;
+  }
+  case GameState::Pregame:
+  {
+    if (screenNeedsUpdate)
+    {
+      showPregame();
+      screenNeedsUpdate = false;
+      Serial.println(screenNeedsUpdate);
+    }
+
+    break;
+  }
+  }
+
+  if (pregameCounter != oldPregameCounter) {
+    oldPregameCounter = pregameCounter;
+    partialUpdates();
+  }
+}
+
+void checkInput()
+{
+  switch (State)
+  {
+  case GameState::None:
+  {
+    uint8_t buttonState = digitalRead(buttonPin);
+
+    if (buttonState == LOW)
+      startCountdown();
+
+    break;
+  }
   }
 }
 
@@ -113,18 +154,87 @@ void showScores()
           bombCountdownTime % 60);
   sprintf(lines[2], "CODE:        %s", bombPassword);*/
 
+  printWithAnimation(lines);
+}
+
+void showPregame()
+{
+  char lines[LCD_ROWS][LCD_COLS + 1] = {{"   - HAZIRLANIN -   "},
+                                        {"Led kirmizi olunca  "},
+                                        {"birakin, yesil olun-"},
+                                        {"ca basin.        5sn"}};
 
   printWithAnimation(lines);
 }
 
-void printWithAnimation(char bucket[LCD_ROWS][LCD_COLS + 1]) {
+void printWithAnimation(char bucket[LCD_ROWS][LCD_COLS + 1])
+{
   lcd.clear();
 
-  for (uint8_t row = 0; row < LCD_ROWS; row++) {
-    for (uint8_t col = 0; col < LCD_COLS; col++) {
+  for (uint8_t row = 0; row < LCD_ROWS; row++)
+  {
+    for (uint8_t col = 0; col < LCD_COLS; col++)
+    {
       lcd.setCursor(col, row);
       lcd.print(bucket[row][col]);
       delay(1);
+    }
+  }
+}
+
+void startCountdown()
+{
+  State = GameState::Pregame;
+  screenNeedsUpdate = true;
+  pregameCounter = PREGAME_COUNTER;
+}
+
+void startGame()
+{
+  State = GameState::Running;
+  screenNeedsUpdate = true;
+}
+
+void setupTimer()
+{
+  noInterrupts();
+  // Clear registers
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+
+  // 1 Hz (16000000/((15624+1)*1024))
+  OCR1A = 15624;
+  // CTC
+  TCCR1B |= (1 << WGM12);
+  // Prescaler 1024
+  TCCR1B |= (1 << CS12) | (1 << CS10);
+  // Output Compare Match A Interrupt Enable
+  TIMSK1 |= (1 << OCIE1A);
+  interrupts();
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+  if (State == GameState::Pregame)
+  {
+    if (pregameCounter > 0)
+      pregameCounter--;
+
+    if (pregameCounter == 0)
+      startGame();
+  }
+}
+
+void partialUpdates()
+{
+  if (screenNeedsUpdate == false)
+  {
+    if (State == GameState::Pregame)
+    {
+      lcd.setCursor(17, 3);
+      lcd.print(pregameCounter);
+      lcd.print("sn");
     }
   }
 }
