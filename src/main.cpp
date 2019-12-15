807890abf78766c9a4b415d887bce6d6e8a85ac7
panalgin <daflan.official@gmail.com>
2019-12-15 12:58:37 +0300

    first test

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 
 LiquidCrystal_I2C lcd(LCD_I2C_ADDR, LCD_COLS, LCD_ROWS);
 
+char brandFirst[LCD_COLS + 1]  = "    REFLEX GAME     ";
+char brandSecond[LCD_COLS + 1] = "      v: 0.9b       ";
+
 const uint8_t redPin = 10;
 const uint8_t greenPin = 11;
 const uint8_t bluePin = 12;
@@ -22,7 +25,28 @@ void setup() {
   lcd.clear();
   lcd.setCursor(0, 0);
 
-  // put your setup code here, to run once:
+  watermark();
+}
+
+void watermark() {
+  lcd.clear();
+  lcd.home();
+
+  for (uint8_t i = 0; i < LCD_COLS; i++) {
+    lcd.setCursor(i, 1);
+    lcd.write(brandFirst[i]);
+
+    if (brandFirst[i] != ' ') delay(75);
+  }
+
+  for (uint8_t i = 0; i < LCD_COLS; i++) {
+    lcd.setCursor(i, 2);
+    lcd.write(brandSecond[i]);
+
+    if (brandSecond[i] != ' ') delay(75);
+  }
+
+  delay(2000);
 }
 
 void loop() {
